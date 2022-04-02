599848eddb8550d449b58d9259ef81286dc371e2
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-04-02 12:15:44 +0530

    feat : added basic routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import { Navbar } from "./components/Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import { Home, Pomodoro, Todo } from "./pages/index";
 function App() {
-  return <div className="App">
-  <Navbar/>
+  return (
+    <div className="App">
+      <Navbar />
 
-  </div>;
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pomodoro" element={<Pomodoro />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React, { StrictMode } from 'react';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { StrictMode } from "react";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import * as ReactDOMClient from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
-const root = ReactDOMClient.createRoot(document.getElementById('root'));
+const root = ReactDOMClient.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </StrictMode>
 );
 
