d53fdf458e19028909fa6641e71c2fbc1f9c97a2
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-04-14 14:30:53 +0530

    fix : path

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import "./Navbar.css";
 import { MdOutlineNightlightRound } from "react-icons/md";
+import { Link } from "react-router-dom";
 export const Navbar = () => {
   return (
     <div className="navbar">
-      <h1 className="navbar-heading">pomodoro</h1>
+      <h1>
+        <Link to="/" className="navbar-heading">
+          pomodoro
+        </Link>
+      </h1>
 
-      <div className="navbar-modes"><MdOutlineNightlightRound/></div>
+      <div className="navbar-modes">
+        <MdOutlineNightlightRound />
+      </div>
     </div>
   );
 };
