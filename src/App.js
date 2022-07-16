720ef4bf41601bc52a3e758d5c59a9f0a18c61ab
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-07-16 10:28:20 +0530

    deleted : all files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import "./App.css";
-import { Navbar } from "./components/Navbar/Navbar";
-import { Routes, Route } from "react-router-dom";
-import { Home, Pomodoro, Todo } from "./pages/index";
+
 function App() {
   return (
     <div className="App">
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pomodoro" element={<Pomodoro />} />
-        <Route path="/todo" element={<Todo />} />
-      </Routes>
+      
     </div>
   );
 }
--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import { Tasks } from "./Tasks/Tasks";
-import { Navbar } from "./Navbar/Navbar";
-import { Modal } from "./Modal/Modal";
-import { Task } from "./Task/Task";
-import { PomodoroTimer } from "./PomodoTimer/PomodoroTimer";
-import { PomodoroDescription } from "./PomodoroDescription/PomodoroDescription";
-
-export { Tasks, Navbar, Modal, Task, PomodoroTimer, PomodoroDescription };
