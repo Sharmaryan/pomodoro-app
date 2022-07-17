fcd35a66239cf9118677732a693fcab1c1ff9060
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-07-17 13:29:46 +0530

    feat : task count

--- src/Pages/Todo/Todo.js
+++ src/Pages/Todo/Todo.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Tasks, Modal } from "../../Components";
 import { useModal } from "../../context/modal-context";
+import { useTodo } from "../../context/todo-context";
 import "./Todo.css";
 export const Todo = () => {
   const { modal } = useModal();
+  const {todoList} = useTodo();
+  const taskLeft = (todoList.length > 0) ? `${todoList.length} tasks left` : `No, task left` ;
   return (
     <div className="todo flex justify-center items-center flex-col">
       {modal && <Modal />}
       <div className="todo-greet font-size-3">welcome back</div>
-      <div className="todo-tasks font-size-3">task left</div>
+      <div className="todo-tasks font-size-3"> {taskLeft}</div>
       <Tasks />
     </div>
   );
