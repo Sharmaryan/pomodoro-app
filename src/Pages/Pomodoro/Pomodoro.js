a9c9ba38d8bbf1b77d7e6607c7d644fa56f4fa09
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-07-17 13:55:24 +0530

    fix : no task bug

--- src/Pages/Pomodoro/Pomodoro.js
+++ src/Pages/Pomodoro/Pomodoro.js
@@ -2,16 +2,17 @@ import React from "react";
 import { useTodo } from "../../context/todo-context";
 import { useParams } from "react-router-dom";
 import { PomodoroTimer, PomodoroDescription } from "../../Components";
-import './Pomodoro.css';
+import "./Pomodoro.css";
 export const Pomodoro = () => {
   const { todoList } = useTodo();
   const { taskId } = useParams();
 
   const currentTask = todoList.filter((todo) => todo.id === taskId);
+
   return (
     <div className="flex items-center pomodoro">
-      <PomodoroTimer task={currentTask} />
-      <PomodoroDescription task={currentTask} />
+      {todoList.length > 0 && <PomodoroTimer task={currentTask} />}
+      {todoList.length > 0 && <PomodoroDescription task={currentTask} />}
     </div>
   );
 };
